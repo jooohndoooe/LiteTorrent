e522d8c121277b29d9fdd6a4d352bace1bd1a1e1
Ustyn <ustin.andreevich@gmail.com>
2024-10-29 15:15:00 +0200

    Fixed ConsoleUI not working

--- LiteTorrent/Program.cs
+++ LiteTorrent/Program.cs
@@ -1,15 +1,37 @@
 ﻿using LiteTorrent.UserInterface.ConsoleUI;
 using LiteTorrent.UserInterface;
 using LiteTorrent.UserInterface.WebUI;
+using LiteTorrent.TorrentManagerServices;
+using MonoTorrent.Client;
+using LiteTorrent.AppConfig;
+using System.Net;
 
 
 const int httpListeningPort = 55125;
 
 var services = new ServiceCollection();
 
+services.AddAppSettings();
+services.AddSingleton(
+            new EngineSettingsBuilder
+            {
+                AllowPortForwarding = true,
+                AutoSaveLoadDhtCache = true,
+                AutoSaveLoadFastResume = true,
+                ListenEndPoints = new Dictionary<string, IPEndPoint> {
+                                { "ipv4", new IPEndPoint (IPAddress.Any, 55123) },
+                                { "ipv6", new IPEndPoint (IPAddress.IPv6Any, 55123) }
+                            },
+                DhtEndPoint = new IPEndPoint(IPAddress.Any, 55123),
+                HttpStreamingPrefix = $"http://127.0.0.1:{httpListeningPort}/"
+            }
+            );
+services.AddSingleton(provider => new ClientEngine(provider.GetRequiredService<EngineSettingsBuilder>().ToSettings()));
+services.AddSingleton<TorrentManagerService>();
 services.AddKeyedSingleton<IUserInterface, ConsoleUserInterface>("cli");
 services.AddKeyedSingleton<IUserInterface, WebUserInterface>("web");
 await using var provider = services.BuildServiceProvider();
 
-var consoleUI = provider.GetRequiredKeyedService<IUserInterface>("web");
-await consoleUI.Run();
+
+var ui = provider.GetRequiredKeyedService<IUserInterface>(args[0]);
+await ui.Run();
